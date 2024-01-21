7e6e76de0df6ea82ff4552484e2e64e552cd539d
dhruv <dhruv21.dk@gmail.com>
2024-01-21 17:45:09 +0530

    search and debouncing done

--- app/api/prompt/route.ts
+++ app/api/prompt/route.ts
@@ -12,3 +12,14 @@ export const POST = async (request: any)=>{
         return new Response("Failed to create a new prompt", { status: 500 });
     }
 }
+
+export const GET = async (request: any)=>{
+    // const payload = await request.json();
+    try{
+       await connectToDb();
+       const data = await prompt.find({});
+       return new Response(JSON.stringify(data),{status: 200});
+    }catch{
+        return new Response("Failed to create a new prompt", { status: 500 });
+    }
+}
--- components/feed.tsx
+++ components/feed.tsx
@@ -1,9 +1,60 @@
-import React from 'react'
-
+"use client";
+import React, { useEffect, useState } from "react";
+let searchTimeout: any;
 const Feed = () => {
-  return (
-    <div>All Feeds</div>
-  )
-}
+  const [searchText, setSearchText] = useState("");
+  const [promptData, setPromptData] = useState<any | null>(null);
+  const [filteredData, setFilteredData] = useState<any | null>(null);
 
-export default Feed
+  useEffect(() => {
+    clearTimeout(searchTimeout);
+    searchTimeout = setTimeout(() => {
+      fetchFilteredData();
+    }, 500);
+    
+  }, [searchText]);
+
+  const fetchFilteredData = () => {
+    console.log('hello')
+    const regex = new RegExp(searchText, "i"); // 'i' flag for case-insensitive search
+    const data = promptData
+      ? promptData?.filter(
+          (item: any) =>
+            regex.test(item.creator.username) ||
+            regex.test(item.tag) ||
+            regex.test(item.prompt)
+        )
+      : null;
+      setFilteredData(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  console.log(filteredData);
+  const fetchData = async () => {
+    try {
+      const response = await fetch("api/prompt", {
+        method: "GET",
+      });
+      const data = await response.json();
+      setPromptData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log(searchText);
+  return (
+    <div className="flex flex-col w-full items-center mt-5">
+      <input
+        className="xs:w-full md:w-1/2 mb-4 p-2 border border-gray-300 rounded"
+        type="text"
+        placeholder="Enter the tag"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default Feed;
